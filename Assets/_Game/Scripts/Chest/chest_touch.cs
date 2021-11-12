89e7dc1a0507a9ea908f8f470a40a25b89bc5f66
Quang-Vu <lqvu.zen@gmail.com>
2021-11-12 14:22:26 +0700

    - create pretab from main scene

---
+++ Assets/_Game/Scripts/Chest/chest_touch.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chest_touch : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D collider = collision.collider;
+
+    }
+}
