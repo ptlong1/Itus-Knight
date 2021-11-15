61638d9caf94c1c130682477bbf48d85ddbe9599
Quang-Vu <lqvu.zen@gmail.com>
2021-11-15 22:57:25 +0700

    - add chest

--- Assets/_Game/Scripts/Chest/chest_touch.cs
+++ Assets/_Game/Scripts/Chest/chest_touch.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class chest_touch : MonoBehaviour
 {
+    public Sprite openedChest;
+    public List<Object> dropItems;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Collider2D collider = collision.collider;
+        if (collision.transform.tag == "Player")
+        {
+            SpriteRenderer spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = openedChest;
+            
+            Destroy(this.gameObject.GetComponent<Collider2D>());
+            Destroy(this.transform.GetChild(0).gameObject);
 
+            //int random = Random.Range(0, dropItems.Count - 1);
+            //Instantiate(dropItems[random], this.transform.position, this.transform.rotation);
+        }    
     }
 }
